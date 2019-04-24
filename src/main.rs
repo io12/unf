4e01f4da069014beb76e9063da9eadb6af09e8cc
Benjamin Levy <blevy@protonmail.com>
2019-04-24 19:22:49 -0400

    refactor and expand features

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@
 extern crate lazy_static;
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate failure;
 extern crate regex;
+extern crate walkdir;
 
 use regex::Regex;
-use std::io::Error;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
+
+type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
 fn unixize_filename_str(fname: &str) -> String {
     lazy_static! {
@@ -22,27 +27,70 @@ fn unixize_filename_str(fname: &str) -> String {
     s.to_string()
 }
 
-fn main() -> Result<(), Error> {
-    let matches = app_from_crate!()
+fn parse_args() -> clap::ArgMatches<'static> {
+    app_from_crate!()
         // TODO: make usage more descriptive
         .args_from_usage(
             "<PATH>... 'The paths of filenames to unixize'
              -r --recursive 'Recursively unixize filenames in directories'
              -q --quiet 'Do not write to stdout'
-             -d --dryrun 'Do not rename files, only print names'",
+             -d --dryrun 'Do not rename files, only print names'
+             -s --follow-symlinks 'Follow symbolic links'",
         )
-        .get_matches();
+        .get_matches()
+}
 
-    let cwd = std::env::current_dir()?;
+fn unixize_filename(path: &Path, args: &clap::ArgMatches<'static>) -> Result<()> {
+    lazy_static! {
+        static ref CWD: PathBuf = std::env::current_dir().unwrap();
+    }
+    let parent = path.parent().unwrap_or(&CWD);
+    let basename = &path
+        .file_name()
+        .ok_or(format_err!("path '{}' has no basename", path.display()))?
+        .to_string_lossy();
+    let new_basename = unixize_filename_str(basename);
 
-    // Here unwrap() is safe because PATH is a required argument
-    for path in matches.values_of("PATH").unwrap().map(Path::new) {
-        let parent = path.parent().unwrap_or(&cwd);
-        let basename = &path.file_name().unwrap().to_string_lossy();
-        let new_basename = unixize_filename_str(basename);
-        let new_path = parent.join(new_basename);
-        println!("{:?}", new_path);
+    if args.is_present("recursive") {
+        let follow_links = args.is_present("follow_symlinks");
+        let dir_iter = WalkDir::new(path).follow_links(follow_links);
+        for ent in dir_iter {
+            unixize_filename(ent?.path(), args)?;
+        }
+    }
+
+    // Skip files that already have unix-friendly names; this is done
+    // after recursive handling because unix-friendly directory names
+    // might have non-unix-friendly filenames inside
+    if basename != &new_basename {
+        return Ok(());
+    }
+
+    let new_path = parent.join(new_basename);
+    if !args.is_present("quiet") {
+        println!("'{}' -> '{}'", path.display(), new_path.display());
+    }
+    if !args.is_present("dryrun") {
+        std::fs::rename(path, new_path)?;
     }
 
     Ok(())
+}
+
+fn try_main() -> Result<()> {
+    let args = parse_args();
+
+    // Here unwrap() is safe because PATH is a required argument
+    for path in args.values_of("PATH").unwrap().map(Path::new) {
+        unixize_filename(path, &args)?;
+    }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = try_main() {
+        eprintln!("Error: {}", err);
+        std::process::exit(1);
+    }
 }
