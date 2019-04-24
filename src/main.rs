1fe138f3f6e3982a3defba11018affe801e48d70
Benjamin Levy <blevy@protonmail.com>
2019-04-24 19:54:18 -0400

    add unit test

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,35 @@ use walkdir::WalkDir;
 
 type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unixize_filename_str() {
+        let f = unixize_filename_str;
+        assert_eq!(f("verbatim"), "verbatim");
+        assert_eq!(f("__trim____"), "trim");
+        assert_eq!(f("__a___b___c__"), "a_b_c");
+        assert_eq!(f("  a   b   c  "), "a_b_c");
+        assert_eq!(f("a-b-c"), "a-b-c");
+        assert_eq!(f("🤔😀😃😄😁😆😅emojis.txt"), "emojis.txt");
+        assert_eq!(f("Game (Not Pirated 😉).rar"), "Game_Not_Pirated.rar");
+    }
+}
+
 fn unixize_filename_str(fname: &str) -> String {
     lazy_static! {
         static ref RE_INVAL_CHR: Regex = Regex::new("[^a-zA-Z0-9._-]").unwrap();
         static ref RE_UND_DUP: Regex = Regex::new("_+").unwrap();
+        static ref RE_UND_DOT: Regex = Regex::new("_+\\.").unwrap();
     }
     // Replace all invalid characters with underscores
     let s = RE_INVAL_CHR.replace_all(fname, "_");
     // Remove duplicate underscores
     let s = RE_UND_DUP.replace_all(&s, "_");
+    // Remove underscores before dot ('.')
+    let s = RE_UND_DOT.replace_all(&s, ".");
     // Remove leading and trailing underscores
     let s = s.trim_matches('_');
     s.to_string()
