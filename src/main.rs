36f22d580c6cf6e1d034c34a37b045cbcd95b27f
Benjamin Levy <blevy@protonmail.com>
2019-06-17 21:25:06 -0400

    handle collisions

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,21 @@ use std::path::{Path, PathBuf};
 
 type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
+#[derive(PartialEq, Debug)]
+struct FilenameParts {
+    stem: String,
+    num: Option<u32>,
+    ext: Option<String>,
+}
+
+const FILENAME_NUM_DIGITS: usize = 3;
+
 #[cfg(test)]
 mod tests {
+    extern crate tempdir;
+
+    use tempdir::TempDir;
+
     use super::*;
 
     #[test]
@@ -29,6 +42,170 @@ mod tests {
         assert_eq!(f("🤔😀😃😄😁😆😅emojis.txt"), "emojis.txt");
         assert_eq!(f("Game (Not Pirated 😉).rar"), "Game_Not_Pirated.rar");
         assert_eq!(f("--fake-flag"), "fake-flag");
+    }
+
+    #[test]
+    fn test_split_filename() {
+        assert_eq!(
+            split_filename("a"),
+            FilenameParts {
+                stem: "a".to_string(),
+                num: None,
+                ext: None,
+            }
+        );
+        assert_eq!(
+            split_filename("a."),
+            FilenameParts {
+                stem: "a".to_string(),
+                num: None,
+                ext: Some("".to_string()),
+            }
+        );
+        assert_eq!(
+            split_filename(".a"),
+            FilenameParts {
+                stem: "".to_string(),
+                num: None,
+                ext: Some("a".to_string()),
+            }
+        );
+        assert_eq!(
+            split_filename("a_0000"),
+            FilenameParts {
+                stem: "a_0000".to_string(),
+                num: None,
+                ext: None,
+            }
+        );
+        assert_eq!(
+            split_filename("a_137"),
+            FilenameParts {
+                stem: "a".to_string(),
+                num: Some(137),
+                ext: None,
+            }
+        );
+        assert_eq!(
+            split_filename("a_000.txt"),
+            FilenameParts {
+                stem: "a".to_string(),
+                num: Some(0),
+                ext: Some("txt".to_string()),
+            }
+        );
+        assert_eq!(
+            split_filename("a____000.txt"),
+            FilenameParts {
+                stem: "a___".to_string(),
+                num: Some(0),
+                ext: Some("txt".to_string()),
+            }
+        );
+        assert_eq!(
+            split_filename(".x._._._222.txt"),
+            FilenameParts {
+                stem: ".x._._.".to_string(),
+                num: Some(222),
+                ext: Some("txt".to_string()),
+            }
+        );
+    }
+
+    #[test]
+    fn test_merge_filename() {
+        assert_eq!(
+            "a",
+            merge_filename(&FilenameParts {
+                stem: "a".to_string(),
+                num: None,
+                ext: None,
+            })
+        );
+        assert_eq!(
+            "a.",
+            merge_filename(&FilenameParts {
+                stem: "a".to_string(),
+                num: None,
+                ext: Some("".to_string()),
+            })
+        );
+        assert_eq!(
+            ".a",
+            merge_filename(&FilenameParts {
+                stem: "".to_string(),
+                num: None,
+                ext: Some("a".to_string()),
+            })
+        );
+        assert_eq!(
+            "a_0000",
+            merge_filename(&FilenameParts {
+                stem: "a_0000".to_string(),
+                num: None,
+                ext: None,
+            })
+        );
+        assert_eq!(
+            "a_137",
+            merge_filename(&FilenameParts {
+                stem: "a".to_string(),
+                num: Some(137),
+                ext: None,
+            })
+        );
+        assert_eq!(
+            "a_000.txt",
+            merge_filename(&FilenameParts {
+                stem: "a".to_string(),
+                num: Some(0),
+                ext: Some("txt".to_string()),
+            })
+        );
+        assert_eq!(
+            "a____000.txt",
+            merge_filename(&FilenameParts {
+                stem: "a___".to_string(),
+                num: Some(0),
+                ext: Some("txt".to_string()),
+            })
+        );
+        assert_eq!(
+            ".x._._._222.txt",
+            merge_filename(&FilenameParts {
+                stem: ".x._._.".to_string(),
+                num: Some(222),
+                ext: Some("txt".to_string()),
+            })
+        );
+    }
+
+    #[test]
+    fn test_resolve_collision() {
+        let tmp_dir = TempDir::new("").unwrap().into_path();
+
+        // Helper function taking a collider filename returning a
+        // string representing the resolved collision
+        let f = |filename: &str| -> String {
+            let path = tmp_dir.join(filename);
+            std::fs::File::create(&path).unwrap();
+
+            resolve_collision(&path)
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
+        };
+
+        assert_eq!(f("a"), "a_000");
+        assert_eq!(f("b_000"), "b_001");
+        assert_eq!(f("c.txt"), "c_000.txt");
+        assert_eq!(f("d_333.txt"), "d_334.txt");
+        assert_eq!(f("e_999.txt"), "e_1000.txt");
+        assert_eq!(f("e_1000.txt"), "e_1000_000.txt");
+        assert_eq!(f("z___222.txt"), "z___223.txt");
+        assert_eq!(f(".x._._._222.txt"), ".x._._._223.txt");
     }
 }
 
@@ -80,12 +257,12 @@ fn unixize_filename(path: &Path, args: &clap::ArgMatches<'static>) -> Result<()>
         .to_string_lossy();
     let new_basename = unixize_filename_str(basename);
 
-    let metadata = if args.is_present("follow-symlinks") {
+    let stat = if args.is_present("follow-symlinks") {
         std::fs::metadata(path)
     } else {
         std::fs::symlink_metadata(path)
     }?;
-    let is_dir = metadata.is_dir();
+    let is_dir = stat.is_dir();
     let should_prompt = !args.is_present("force");
 
     let recurse = args.is_present("recursive")
@@ -108,6 +285,7 @@ fn unixize_filename(path: &Path, args: &clap::ArgMatches<'static>) -> Result<()>
     }
 
     let new_path = parent.join(new_basename);
+    let new_path = resolve_collision(&new_path);
     let msg = format!("rename '{}' -> '{}'", path.display(), new_path.display());
     if should_prompt {
         // Interactively prompt whether to rename the file, skipping
@@ -119,9 +297,75 @@ fn unixize_filename(path: &Path, args: &clap::ArgMatches<'static>) -> Result<()>
     } else {
         println!("{}", msg);
     }
-    std::fs::rename(path, new_path)?;
 
+    std::fs::rename(path, new_path)?;
     Ok(())
+}
+
+// Check if the target path can be written to without clobbering an
+// existing file. If it can't, change it to a unique name. Note that
+// this function requires that the filename is non-empty and valid
+// UTF-8.
+//
+// TODO: Make sure this invariant holds
+fn resolve_collision(path: &Path) -> PathBuf {
+    if path.exists() {
+        let filename = path
+            .file_name()
+            .expect("filename is empty")
+            .to_str()
+            .expect("filename is not valid UTF-8");
+        let FilenameParts { stem, num, ext } = split_filename(filename);
+        let num = match num {
+            Some(val) => Some(val + 1),
+            None => Some(0),
+        };
+        let filename = merge_filename(&FilenameParts { stem, num, ext });
+        let path = path.with_file_name(filename);
+        resolve_collision(&path)
+    } else {
+        path.to_path_buf()
+    }
+}
+
+fn split_filename(filename: &str) -> FilenameParts {
+    let mut it = filename.rsplitn(2, '.');
+    let ext = it.next().expect("tried to split empty filename");
+    let maybe_stem_num = it.next();
+    let (stem_num, ext) = match maybe_stem_num {
+        Some(stem_num) => (stem_num, Some(ext.to_string())),
+        None => (ext, None),
+    };
+    let num_it = stem_num.chars().rev().take(4).collect::<Vec<_>>();
+    let mut num_it = num_it.iter().rev();
+    let num = if num_it.next() == Some(&'_') && num_it.len() == FILENAME_NUM_DIGITS {
+        num_it.collect::<String>().parse::<u32>().ok()
+    } else {
+        None
+    };
+    let stem = if num.is_some() {
+        stem_num
+            .chars()
+            .take(stem_num.len() - FILENAME_NUM_DIGITS - 1)
+            .collect()
+    } else {
+        stem_num.to_string()
+    };
+    FilenameParts { stem, num, ext }
+}
+
+fn merge_filename(parts: &FilenameParts) -> String {
+    let mut s = String::new();
+    s.push_str(&parts.stem);
+    if let Some(num) = parts.num {
+        s.push('_');
+        s.push_str(&format!("{:0width$}", num, width = FILENAME_NUM_DIGITS));
+    }
+    if let Some(ref ext) = parts.ext {
+        s.push('.');
+        s.push_str(ext);
+    }
+    s
 }
 
 fn try_main() -> Result<()> {
