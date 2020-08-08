eb1eec7393ec77ff91841c5c74b4bc3a5bffb70e
Benjamin Levy <blevy@protonmail.com>
2020-08-07 23:16:50 -0400

    Comment mem_fs::load_insert()

--- src/mem_fs.rs
+++ src/mem_fs.rs
@@ -7,9 +7,12 @@ use std::path::PathBuf;
 
 use rsfs::GenFS;
 
+/// Load file or directory from `path` into `fs`, including all children.
 fn load_insert(fs: &rsfs::mem::FS, path: &Path) -> Result<()> {
     if path.is_dir() {
         fs.create_dir(path)?;
+
+        // Load children
         for ent in path.read_dir()? {
             let path = ent?.path();
             load_insert(fs, &path)?;
